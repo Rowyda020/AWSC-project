4433dce4c81326478296f2fa209941d9187e34a1
Ahmed Sherif <ahmedsherif91101@gmail.com>
2023-08-22 14:58:42 +0300

    "report-add-item 22 8 2023"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -64,9 +64,9 @@ import { STREmployeeOpeningCustodyTableComponent } from './str-employee-opening-
 import { STREmployeeOpeningCustodyDialogComponent } from './str-employee-opening-custody-dialog/str-employee-opening-custody-dialog.component';
 import { STRGroup1Component } from './str-group1/str-group1.component';
 import { STRGroup1DialogComponent } from './str-group1-dialog/str-group1-dialog.component';
-import {StrWithdrawContainerComponent} from './str-withdraw-container/str-withdraw-container.component'
-import {  StrWithdrawDialogComponent} from "./str-withdraw-dialog2/str-withdraw-dialog2.component";
-import {StrWithdrawTableComponent  } from "./str-withdraw-table2/str-withdraw-table2.component";
+import { StrWithdrawContainerComponent } from './str-withdraw-container/str-withdraw-container.component';
+import { StrWithdrawDialogComponent } from './str-withdraw-dialog2/str-withdraw-dialog2.component';
+import { StrWithdrawTableComponent } from './str-withdraw-table2/str-withdraw-table2.component';
 import { StrProductComponent } from './str-product/str-product.component';
 import { StrProductDialogComponent } from './str-product-dialog/str-product-dialog.component';
 import { STRItem1Component } from './str-item1/str-item1.component';
@@ -76,7 +76,7 @@ import { FIAccountDialogComponent } from './fi-account-dialog/fi-account-dialog.
 import { FiEntryContainerComponent } from './fi-entry-container/fi-entry-container.component';
 import { FiEntryTableComponent } from './fi-entry-table/fi-entry-table.component';
 import { FiEntryDialogComponent } from './fi-entry-dialog/fi-entry-dialog.component';
-import {  FileUploadComponent} from "./file-upload/file-upload.component";
+import { FileUploadComponent } from './file-upload/file-upload.component';
 import { FileUploadDialogComponent } from './file-upload-dialog/file-upload-dialog.component';
 import { FIAccountHierarchyComponent } from './fi-account-hierarchy/fi-account-hierarchy.component';
 import { FIAccountHierarchyDialogComponent } from './fi-account-hierarchy-dialog/fi-account-hierarchy-dialog.component';
@@ -86,10 +86,13 @@ import { StrReportAddItemComponent } from './str-report-add-item/str-report-add-
 import { FiAccountItemComponent } from './fi-account-item/fi-account-item.component';
 import { FiAccountItemdDialogComponent } from './fi-account-itemd-dialog/fi-account-itemd-dialog.component';
 
-
 @NgModule({
   declarations: [
-    AppComponent,StrWithdrawContainerComponent,StrWithdrawDialogComponent,StrWithdrawTableComponent,FileUploadComponent,
+    AppComponent,
+    StrWithdrawContainerComponent,
+    StrWithdrawDialogComponent,
+    StrWithdrawTableComponent,
+    FileUploadComponent,
     StrCommodityComponent,
     StrCommodityDialogComponent,
     LoginComponent,
@@ -137,17 +140,10 @@ import { FiAccountItemdDialogComponent } from './fi-account-itemd-dialog/fi-acco
     FIAccountHierarchyComponent,
     FIAccountHierarchyDialogComponent,
 
-    StrReportAddItemComponent
+    StrReportAddItemComponent,
 
     FiAccountItemComponent,
-    FiAccountItemdDialogComponent
-
-    
-
-
-
-
-
+    FiAccountItemdDialogComponent,
   ],
   imports: [
     BrowserModule,
