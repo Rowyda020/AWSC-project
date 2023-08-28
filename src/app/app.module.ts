39c2be3c3f923047522931597e24a81045867446
Feeby <Feeby@ISDT.local>
2023-08-28 10:54:35 +0300

    ........

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -112,6 +112,9 @@ import { HrMillitryStateComponent } from './hr-millitry-state/hr-millitry-state.
 import { HrMillitryStateDialogComponent } from './hr-millitry-state-dialog/hr-millitry-state-dialog.component';
 import { HrVacationComponent } from './hr-vacation/hr-vacation.component';
 import { HrVacationDailogComponent } from './hr-vacation-dailog/hr-vacation-dailog.component';
+import { HrQualificationLevelComponent } from './hr-qualification-level/hr-qualification-level.component';
+import { HrQualificationLevelDialogComponent } from './hr-qualification-level-dialog/hr-qualification-level-dialog.component';
+
 
 @NgModule({
   declarations: [
@@ -189,7 +192,10 @@ import { HrVacationDailogComponent } from './hr-vacation-dailog/hr-vacation-dail
     HrMillitryStateComponent,
     HrMillitryStateDialogComponent,
     HrVacationComponent,
-    HrVacationDailogComponent
+    HrVacationDailogComponent,
+    HrQualificationLevelComponent,
+    HrQualificationLevelDialogComponent,
+   
   
 
 
